53499106668943e30be0e7d67ce9b198390b6833
Chrystian <chrystianralfc@gmail.com>
2024-03-08 19:10:16 -0400

    ajustando estilos y siendo sitio estatico

--- en/script.js
+++ en/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (widthWindow < 425) {
         let titleName = document.querySelector(".title-name");
         titleName.style.display = "none";
-        nav.style.top = -150 + "px";
+        nav.style.top = -250 + "px";
     }
 
     // When you click on the header menu
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (parseInt(nav.style.top) < 0) {
             nav.style.top = 50 + "px";
         } else {
-            nav.style.top = -150 + "px";
+            nav.style.top = -250 + "px";
         }
     });
 
@@ -23,14 +23,30 @@ document.addEventListener("DOMContentLoaded", () => {
     let options = document.querySelectorAll(".option");
     options.forEach(option => {
         option.addEventListener('click', () => {
-            nav.style.top = -150 + "px";
+            nav.style.top = -250 + "px";
         });
     });
 
-    // 
+    // show message
     let messageDOM = document.querySelector(".message");
-    messageDOM.style.top = "10px";
-    let timeMessage = setInterval(function () {
-        messageDOM.style.top = "-100px";
-    }, 5000);
+    if (messageDOM !== null) {
+        messageDOM.style.top = "10px";
+        let timeMessage = setInterval(function () {
+            messageDOM.style.top = "-100px";
+        }, 5000);
+    }
+
+    // show language
+    let divLanguage = document.getElementById("selectLanguage");
+    let ulLanguage = document.querySelector("#selectLanguage > ul");
+    divLanguage.addEventListener("click", () => {
+        if (ulLanguage.classList.contains("visible")) {
+            ulLanguage.classList.remove("visible");
+        } else {
+            ulLanguage.classList.add("visible");
+        }
+    });
+    document.addEventListener("click", (e) => {
+        console.log(e)
+    });
 });
